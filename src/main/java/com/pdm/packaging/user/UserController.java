056936ed59e345c7123b63ca0fc6354f2ac91831
Artem Romanenko <ar5336@g.rit.edu>
2019-04-14 17:06:31 -0700

    joined Business on User

--- src/main/java/com/pdm/packaging/user/UserController.java
+++ src/main/java/com/pdm/packaging/user/UserController.java
@@ -19,14 +19,15 @@ public class UserController {
     public QueryData user(@RequestParam(value="userID", defaultValue = "0") Integer user_ID,
                           @RequestParam(value="name", defaultValue = "") String name,
                           @RequestParam(value="isPremium", defaultValue = "-1") Integer is_premium,
-                          @RequestParam(value="phoneNumber", defaultValue = "0") Integer phone_number,
+                          @RequestParam(value="phoneNumber", defaultValue = "") String phone_number,
                           @RequestParam(value="businessID", defaultValue = "0") Integer business_ID) {
-        String userCall = "select * from users";
+        String userCall =      "select * from users inner join business on users.business_ID=business.business_ID";
+        //String packageCall = "select * from package inner join traits on package.trait=traits.trait_ID";
         LinkedHashMap<String, String> arguments = new LinkedHashMap<>();
         if (user_ID > 0) arguments.put("user_ID", user_ID.toString());
         if (name.length() > 0 && name.length() <= 255) arguments.put("name", name);
         if (is_premium == 0 || is_premium == 1) arguments.put("is_premium", is_premium.toString());
-        if (phone_number > 0) arguments.put("phone_number", phone_number.toString());
+        if (phone_number.length() > 0 && phone_number.length() <= 11) arguments.put("phone_number", phone_number);
         if (business_ID > 0) arguments.put("business_ID", business_ID.toString());
         userCall = h2.buildQuery(userCall, arguments);
         QueryData results = new QueryData();
@@ -34,12 +35,14 @@ public class UserController {
             ResultSet users = h2.query(userCall);
             while (users.next()) {
                 results.addData(new User(users.getInt("user_ID"),
-                        users.getString("name"),
+                        users.getString("users.name"),
                         users.getBoolean("is_premium"),
-                        users.getInt("phone_number"),
-                        users.getInt("business_ID")));
+                        users.getString("phone_number"),
+                        /*users.getInt("business.business_ID"),*/
+                        users.getString("business.name")));
             }
         } catch (SQLException se) {
+            se.printStackTrace();
             results = h2.errorCall(results, userCall);
         }
         return results;

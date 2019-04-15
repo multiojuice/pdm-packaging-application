d43bcc64ca6bcb253e9f4011207e262825859150
CetaceanNation <gamer.72.365@gmail.com>
2019-04-14 20:22:54 -0400

    Tracking joined on transport and location tables

--- src/main/java/com/pdm/packaging/tracking/Tracking.java
+++ src/main/java/com/pdm/packaging/tracking/Tracking.java
@@ -3,7 +3,9 @@ package com.pdm.packaging.tracking;
 public class Tracking {
     private int trackingID;
     private int transportID;
+    private String transportName;
     private int currentlocationID;
+    private String currentlocationName;
 
     public Tracking(int trackingID, int transportID, int currentlocationID) {
         this.trackingID = trackingID;
@@ -11,9 +13,25 @@ public class Tracking {
         this.currentlocationID = currentlocationID;
     }
 
+    public Tracking(int trackingID, int transportID, String transportName, int currentlocationID, String currentlocationName) {
+        this.trackingID = trackingID;
+        this.transportID = transportID;
+        this.transportName = transportName;
+        this.currentlocationID = currentlocationID;
+        this.currentlocationName = currentlocationName;
+    }
+
     public int getTrackingID() { return trackingID; }
 
     public int getTransportID() { return transportID; }
 
+    public String getTransportName() {
+        return transportName;
+    }
+
     public int getcurrentlocationID() { return currentlocationID; }
+
+    public String getCurrentlocationName() {
+        return currentlocationName;
+    }
 }
